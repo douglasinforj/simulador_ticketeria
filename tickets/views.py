00ea2e71749275f9edc7f9d86e550d5fb806f34f
Douglas Silva <douglasitpro@gmail.com>
2025-02-04 21:26:17 -0300

    criando views e endpoints

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators  import action
+from .models import Cliente, Evento, Ingresso
+from .serializers import ClienteSerializer, EventoSerializer, IngressoSerializer
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+
+class EventoViewSet(viewsets.ModelViewSet):
+    queryset = Evento.objects.all()
+    serializer_class = EventoSerializer
+
+class IngressoViewSet(viewsets.ModelViewSet):
+    queryset = Ingresso.objects.all()
+    serializer_class = IngressoSerializer
+
+    @action(detail=False, methods=['get'])
+    def vendidos(self, request):
+        # EndPoint pública
+        ingresso = Ingresso.objects.all()
+        serializer = IngressoSerializer(ingresso, many=True)
+        return Response(serializer.data)
+
