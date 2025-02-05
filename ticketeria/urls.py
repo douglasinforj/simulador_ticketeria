e141e81c1bbeb260faaa8585803915a222b16903
Douglas Silva <douglasitpro@gmail.com>
2025-02-04 22:12:08 -0300

    configurando servir arquvio de media na url

--- ticketeria/urls.py
+++ ticketeria/urls.py
@@ -4,13 +4,17 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from tickets.views import ClienteViewSet, EventoViewSet, IngressoViewSet
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 router = DefaultRouter()
 router.register(r'clientes', ClienteViewSet)
 router.register(r'eventos', EventoViewSet)
-router.register(r'ingrssos', IngressoViewSet)
+router.register(r'ingressos', IngressoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api', include(router.urls)), 
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
